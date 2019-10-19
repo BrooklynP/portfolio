e41eadf87f59f75589b002e7025ae8a1d63536bf
BrooklynP <brooklynpedley@gmail.com>
2019-10-19 19:10:05 +0100

    redesigned single project page. moved some css to global styles. added redirect from single project page if no project selected. Fixed github link in header

--- src/app/pages/single-project-page/single-project-page.component.ts
+++ src/app/pages/single-project-page/single-project-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectsDataService } from 'src/app/portfolioData/projects-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-single-project-page',
@@ -10,10 +11,14 @@ export class SingleProjectPageComponent implements OnInit {
 
   public project: any;
 
-  constructor(public projectDataService: ProjectsDataService) { }
+  constructor(public projectDataService: ProjectsDataService, public router: Router) { }
 
   ngOnInit() {
     this.project = this.projectDataService.getCurrentProject();
+    if (this.project === undefined) {
+      console.warn("There was no project selected");
+      this.router.navigate(['']);
+    }
   }
 
 }
