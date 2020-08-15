6f6170a77f33aed7769653ad734fc268640fa5e5
Brooklyn Pedley <brooklynpedley@gmail.com>
2020-08-15 19:26:52 +0100

    cleaned up dates on work tiles

--- src/app/components/work-tile/work-tile.component.ts
+++ src/app/components/work-tile/work-tile.component.ts
@@ -9,14 +9,21 @@ export class WorkTileComponent implements OnInit {
   @Input() company: string;
   @Input() title: string;
   @Input() startDate: Date;
-  @Input() endDate: Date = new Date();
+  @Input() endDate: Date;
   @Input() duties: Array<string>;
   @Input() techStack: any;
+
+  public endDateProvided = true;
 
   constructor() { }
 
   ngOnInit() {
+    if(!this.endDate){
+      this.endDate = new Date();
+      this.endDateProvided = false;
+    }
   }
+
   getLengthOfTime() {
     let months = 0;
     months = (this.endDate.getFullYear() - this.startDate.getFullYear()) * 12;
