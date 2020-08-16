8e9f79d59190b6209faa1655e59d4905013930db
Brooklyn Pedley <brooklynpedley@gmail.com>
2020-08-16 22:17:44 +0100

    changed landing page to be old school terminal

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -8,15 +8,41 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
   public messageToDisplayLine1 = '';
-  public messageToDisplayLine2 = "";
+  public messageToDisplayLine2 = '';
+  public messageToDisplayLine3 = '';
+  public messageToDisplayLine4 = '';
+  public messageToDisplayLine5 = '';
+  public messageToDisplayLine6 = '';
+  public messageToDisplayLine7 = '';
+  public messageToDisplayLine8 = '';
+  public messageToDisplayLine9 = '';
+  public messageToDisplayLine10 = '';
+  public messageToDisplayLine11 = '';
+  public messageToDisplayLine12 = '';
+  public messageToDisplayLine13 = '';
 
-  private messageLine1 = '>Hi!';
-  private messageLine2 = ">I'm Brooklyn, an aspiring software developer from Hertfordshire";
-  private readonly TypingSpeed = 50;
+  private messageLine1 = 'Tasks: 37 total, 1 running';
+  private messageLine2 = 'Uptime: 00:16:45';
+  private messageLine3 = 'DATE HERE V 1.0.2';
+  private messageLine4 = 'Load Avg. 4.85, 2.84, 2.10 CPU Usage: 38.61% user, 29.70% sys, 31.68% idle';
+  private messageLine5 = 'SharedLibs: 215M resident, 57M data, 23M linkedit.';
+  private messageLine6 = 'MemRegions 318147 total, 5823M resident, 190M private, 2646M shared.';
+  private messageLine7 = 'PhysMem: 16G used (3558M wired), 103M unused.';
+  private messageLine8 = 'VM: 13T vsize, 1299M framework vsize, 47281010(0) swapins, 49223315(0) swapouts.';
+  private messageLine9 = 'Networks: packets: 21230244/27G in, 26612975/3179M out.';
+  private messageLine10 = 'Disks: 5635951/270G read, 4674334/276G written.';
+  private messageLine11 = '> Hi!';
+  private messageLine12 = "> I'm Brooklyn, an aspiring software developer from Hertfordshire";
+  private messageLine13 = " > root@portfolio: $echo 'Click Here To See Portfolio'";
 
-  constructor(public router: Router) {}
+  private readonly TypingSpeed = 5;
+
+  constructor(public router: Router) {
+
+  }
 
   typeLine() {
+    window.scrollTo(0,document.body.scrollHeight);
     if (this.messageLine1.length > 0) {
       setTimeout(() => {
         this.messageToDisplayLine1 += this.messageLine1[0];
@@ -30,6 +56,85 @@ export class LandingPageComponent implements OnInit {
         this.messageLine2 = this.messageLine2.substring(1);
         this.typeLine();
       }, this.TypingSpeed);
+    }else
+    if (this.messageLine3.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine3 += this.messageLine3[0];
+        this.messageLine3 = this.messageLine3.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine4.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine4 += this.messageLine4[0];
+        this.messageLine4 = this.messageLine4.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine5.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine5 += this.messageLine5[0];
+        this.messageLine5 = this.messageLine5.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine6.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine6 += this.messageLine6[0];
+        this.messageLine6 = this.messageLine6.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine7.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine7 += this.messageLine7[0];
+        this.messageLine7 = this.messageLine7.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine8.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine8 += this.messageLine8[0];
+        this.messageLine8 = this.messageLine8.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine9.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine9 += this.messageLine9[0];
+        this.messageLine9 = this.messageLine9.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine10.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine10 += this.messageLine10[0];
+        this.messageLine10 = this.messageLine10.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine11.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine11 += this.messageLine11[0];
+        this.messageLine11 = this.messageLine11.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine12.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine12 += this.messageLine12[0];
+        this.messageLine12 = this.messageLine12.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }else
+    if (this.messageLine13.length > 0) {
+      setTimeout(() => {
+        this.messageToDisplayLine13 += this.messageLine13[0];
+        this.messageLine13 = this.messageLine13.substring(1);
+        this.typeLine();
+      }, this.TypingSpeed);
+    }
+    else{
     }
   }
 
