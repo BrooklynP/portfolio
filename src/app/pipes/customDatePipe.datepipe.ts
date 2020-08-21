68fa2461d4e4928f5e9fee70a457979752d02693
Brooklyn Pedley <brooklynpedley@gmail.com>
2020-08-21 21:59:21 +0100

    cleanup

--- src/app/pipes/customDatePipe.datepipe.ts
+++ src/app/pipes/customDatePipe.datepipe.ts
@@ -7,7 +7,6 @@ import { DatePipe } from '@angular/common';
 export class CustomDatePipe extends
     DatePipe implements PipeTransform {
     transform(value: any, args?: any): any {
-        // console.log(value);
         return super.transform(value, "MMMM y");
     }
 }
